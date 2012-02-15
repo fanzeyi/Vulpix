385b0950611b27c95f7e479382ced761f14b270a
饭团 <fanzeyi1994@gmail.com>
2012-02-15 11:49:43 +0800

    add config example

---
+++ judge/config.example.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+mysql_config = {
+    'mysql_host'     : '', 
+    'mysql_user'     : '', 
+    'mysql_password' : '', 
+    'mysql_database' : ''
+}
+
+accept_lang = {
+    'zh_cn' : 'zh_CN', 
+    'en'    : 'en_US', 
+}
